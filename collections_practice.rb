c8365db712f3c4ead7910b24ec632ef24fa19555
Jonathan Harwood <jhharwood88@gmail.com>
2018-11-29 20:36:18 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,29 @@
+def sort_array_asc(numbers)
+  numbers.sort
+end
+
+def sort_array_desc(numbers)
+  numbers.sort { |x,y| y <=> x }
+end
+
+def sort_array_char_count(words)
+  words.sort_by(&:downcase)
+end
+
+def swap_elements(names)
+names[1], names[2] = names[2], names[1]
+names
+end
+
+def reverse_array(numbers)
+numbers.reverse
+end
+
+def kesha_maker(names)
+changed_names = []  
+names.each { |x| names.repalce }
+
+end
+
+
 
